a68365030c58c08b0a83cf367eb2ccc86a756447
cooperaus <144241777+cooperaus@users.noreply.github.com>
2023-10-11 09:07:27 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,7 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction(X=15):
   # My Function
+X=2000000000 x=5
+print(X/x+X*x/X)
